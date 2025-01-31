ab92d466c6b7b4023f87f986ecb6c1efd0b4aa3e
Alex <alex@pc-19.home>
2025-01-31 15:06:54 +0100

    Ajout du fichier NotificationBanner dans components/

---
+++ src/components/NotificationBanner.vue
@@ -0,0 +1,42 @@
+<template>
+    <div v-if="notifications.length" class="notification-container">
+        <div v-for="note in notifications" :key="note.id" :class="['notification', note.type]">
+            {{ note.message }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useNotificationStore } from '@/stores/notification';
+const { notifications } = useNotificationStore();
+</script>
+
+<style>
+.notification-container {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+}
+
+.notification {
+    background: lightgray;
+    padding: 10px;
+    border-radius: 5px;
+    margin-bottom: 5px;
+}
+
+.notification.info {
+    background: blue;
+    color: white;
+}
+
+.notification.success {
+    background: green;
+    color: white;
+}
+
+.notification.error {
+    background: red;
+    color: white;
+}
+</style>
