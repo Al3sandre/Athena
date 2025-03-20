f9da0adc6563f43fe1a2f24e12c5d98747138631
Alex <alex@fedora.home>
2025-03-20 11:21:17 +0100

    ajout de la possibiliter de suprimer une notif en cliquant dessus

--- src/components/NotificationBanner.vue
+++ src/components/NotificationBanner.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="fixed top-10 right-10 z-50 space-y-4">
-        <div v-for="notification in notifications" :key="notification.id" :class="notificationClass(notification.type)">
+        <div v-for="notification in notifications" :key="notification.id" :class="notificationClass(notification.type)"
+            @click="remove(notification.id)" class="cursor-pointer">
             {{ notification.message }}
         </div>
     </div>
@@ -12,6 +13,11 @@ import { computed } from 'vue';
 
 const notificationStore = useNotificationStore();
 const notifications = computed(() => notificationStore.notifications);
+
+// Méthode pour supprimer une notification
+const remove = (id) => {
+    notificationStore.removeNotification(id);
+};
 
 const notificationClass = (type) => {
     switch (type) {
